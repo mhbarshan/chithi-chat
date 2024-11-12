82ba523a110a674d3c5e12e4e59c15fd579974ce
mhbarshan <mhb247@gmail.com>
2024-11-12 20:13:01 +0600

    build try 2

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ const port = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API running");
-});
+// app.get("/", (req, res) => {
+//   res.send("API running");
+// });
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
