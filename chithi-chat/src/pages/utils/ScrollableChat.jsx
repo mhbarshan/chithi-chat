98fd568b3ee13649b7308f70c6a00444375d5ebd
mhbarshan <mhb247@gmail.com>
2024-11-12 23:21:52 +0600

    build try 5.4.3

--- chithi-chat/src/pages/utils/ScrollableChat.jsx
+++ chithi-chat/src/pages/utils/ScrollableChat.jsx
@@ -17,24 +17,24 @@ const ScrollableChat = ({ messages }) => {
         messages.map((m, i) => (
           <div style={{ display: "flex" }} key={m._id}>
             {!m.chat.isGroupChat
-              ? (!isSameSender(messages, m, i, user._id) ||
-                  (isLastMessage(messages, i, user._id) &&
-                    m.sender._id !== user._id)) && (
-                  <Tooltip
-                    label={m.sender.name}
-                    placement="bottom-start"
-                    hasArrow
-                  >
-                    <Avatar
-                      mt="7px"
-                      mr={1}
-                      size="sm"
-                      cursor="pointer"
-                      name={m.sender.name}
-                      src={m.sender.picture}
-                    />
-                  </Tooltip>
-                )
+              ? !isSameSender(messages, m, i, user._id) ||
+                (isLastMessage(messages, i, user._id) &&
+                  m.sender._id !== user._id && (
+                    <Tooltip
+                      label={m.sender.name}
+                      placement="bottom-start"
+                      hasArrow
+                    >
+                      <Avatar
+                        mt="7px"
+                        mr={1}
+                        size="sm"
+                        cursor="pointer"
+                        name={m.sender.name}
+                        src={m.sender.picture}
+                      />
+                    </Tooltip>
+                  ))
               : m.sender._id !== user._id && (
                   <Tooltip
                     label={m.sender.name}
